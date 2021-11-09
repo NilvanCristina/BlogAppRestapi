fe36454b13af452b82d423d1ca0e14dd1ec44a5e
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-11-09 21:29:04 +0200

    Update model Newsletter

--- Newsletter/models.py
+++ Newsletter/models.py
@@ -5,5 +5,8 @@ class Newsletter(models.Model):
     email = models.EmailField(max_length=200, blank=False)
     date = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-date']
+
     def __str__(self):
         return self.email
