9634c597ed0685af3265826151d32eb172735ff0
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-11-09 20:07:30 +0200

    Update Blog model

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish")
+)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True, blank=False)
+    description = models.TextField(default="")
+    content = models.TextField(blank=False, default="")
+    author = models.CharField(max_length=200, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+    post_status = models.IntegerField(choices=STATUS, default=0)
