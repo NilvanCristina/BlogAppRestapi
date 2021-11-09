81478fe6e2dd2c16f0ba1d1461c30153efeeae4b
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-11-09 20:53:51 +0200

    Update model and admin

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'post_status', 'created_on')
+
+
+admin.site.register(Post, PostAdmin)
+
+admin.site.site_header = "Blog Control Panel"
--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 STATUS = (
@@ -10,7 +11,10 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True, blank=False)
     description = models.TextField(default="")
     content = models.TextField(blank=False, default="")
-    author = models.CharField(max_length=200, blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     post_status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return self.title
