05b4bef36bda091b55297ea079cf5abdeddb0376
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-11-09 21:24:45 +0200

    Update model and admin Newsletter

--- Newsletter/models.py
+++ Newsletter/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Newsletter(models.Model):
+    email = models.EmailField(max_length=200, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        self.email
