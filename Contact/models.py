66252b848b0a75518eef82e77d5313323df27d89
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-11-11 17:32:41 +0200

    Update Contact model

--- Contact/models.py
+++ Contact/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
+SUBJECT = (
+    (0, "Collaboration"),
+    (1, "Meeting"),
+    (2, "Consultancy"),
+    (3, "Gratitude")
+)
+
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=100, blank=False)
     last_name = models.CharField(max_length=100, blank=False)
     email = models.EmailField(max_length=200, blank=False)
+    subject = models.IntegerField(choices=SUBJECT, default=3)
     date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
