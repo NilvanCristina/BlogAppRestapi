a5b7687cab9dbaa96603d7f40fc9c8ae627c9197
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-11-11 22:10:47 +0200

    Update BlogAppRestapi urls

--- BlogAppRestapi/urls.py
+++ BlogAppRestapi/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('newsletter/', include('Newsletter.urls'))
+    path('newsletter/', include('Newsletter.urls')),
+    path('contact/', include('Contact.urls')),
 ]
